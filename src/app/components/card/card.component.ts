6b6f25304c1306d86fb26bcd4ed9fe84f2ac5506
genesisramirez <ateamdev.genesisnr@gmail.com>
2020-04-28 12:48:54 -0400

    actualizacion

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,61 +7,77 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  public start: number = 1;
+  public total : number = 0;
+  public subtotal : Array<number> = [];
   public products : Array<any> = [
     {
       id: 1,
       img: "../../assets/harinapan.jpg",
       name: "Harina Pan 1kg",
-      price: "3$"
+      price: 2,
+      quantity: 1
     },
     {
       id: 2,
       img: "../../assets/harinadetrigo.jpg",
       name: "Harina de trigo Juana 1kg",
-      price: "3$"
+      price: 5,
+      quantity: 1
     },
     {
       id: 3,
       img: "../../assets/mayonesa.png",
       name: "Mayonesa Mavesa 500ml Harina de trigo Juana 1kg",
-      price: "5$"
+      price: 8,
+      quantity: 1
     },
     {
       id: 4,
       img: "../../assets/salsadetomate.jpg",
       name: "Salsa de Tomate Heinz 400ml",
-      price: "3$"
+      price: 4,
+      quantity: 1
     },
     {
       id: 5,
       img: "../../assets/mayonesa.png",
       name: "Mayonesa Mavesa 500ml Harina de trigo Juana 1kg",
-      price: "5$"
+      price: 3,
+      quantity: 1
     },
     {
       id: 6,
       img: "../../assets/salsadetomate.jpg",
       name: "Salsa de Tomate Heinz 400ml",
-      price: "3$"
+      price: 2,
+      quantity: 1
     },
     {
       id: 7,
       img: "../../assets/mayonesa.png",
       name: "Mayonesa Mavesa 500ml Harina de trigo Juana 1kg",
-      price: "5$"
+      price: 1,
+      quantity: 1
     },
     {
       id: 8,
       img: "../../assets/salsadetomate.jpg",
       name: "Salsa de Tomate Heinz 400ml",
-      price: "3$"
+      price: 4,
+      quantity: 1
     },
   ];
 
   constructor(public modalController: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
+
+  ionViewDidEnter(){
+    this.totalAmount()
+  }
+
 
   dismiss() {
     // using the injected ModalController this page
@@ -71,19 +87,43 @@ export class CardComponent implements OnInit {
     });
   }
 
-  quantity(){
+  totalAmount(){
     for(const i in this.products){
-      
+      this.subtotal.push(this.products[i].quantity * this.products[i].price);
+      this.total = this.subtotal.reduce((a, b) => a + b, 0);
+      console.log(this.subtotal)
+      console.log(this.total)
     }
   }
 
-  add(){
-    this.start = this.start + 1;
+  subtotalAmount(){
+    this.subtotal = [];
+    this.total = 0;
+    for(const i in this.products){
+      this.subtotal.push(this.products[i].quantity * this.products[i].price)
+      this.total = this.subtotal.reduce((a, b) => a + b, 0);
+      console.log(this.subtotal)
+      console.log(this.total)
+    }
   }
 
-  remove(){
-    if(this.start > 1){
-      this.start = this.start - 1;
+  add(id){
+    for(const i in this.products){
+      if(this.products[i].id == id){
+        this.products[i].quantity = this.products[i].quantity + 1;
+        this.subtotalAmount()
+      }
+    }
+  }
+
+  remove(id){
+    for(const i in this.products){
+      if(this.products[i].id == id){
+        if(this.products[i].quantity > 1){
+          this.products[i].quantity = this.products[i].quantity - 1;
+          this.subtotalAmount()
+        }
+      }
     }
   }
 
