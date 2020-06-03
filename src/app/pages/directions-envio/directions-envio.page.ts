f782fc9c2c54da9af6411aaa38d1bef6cfef2297
genesisramirez <ateamdev.genesisnr@gmail.com>
2020-06-03 08:07:02 -0400

    actualizacion 03/06/2020

--- src/app/pages/directions-envio/directions-envio.page.ts
+++ src/app/pages/directions-envio/directions-envio.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { DirectionsComponent } from 'src/app/components/directions/directions.component';
 import { ModalController } from '@ionic/angular';
+import { FiltersServicesService} from '../../servicesGenerals/filters-services.service';
+import { DirectionsEnvioService } from './services/directions-envio.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-directions-envio',
@@ -9,12 +12,24 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./directions-envio.page.scss'],
 })
 export class DirectionsEnvioPage implements OnInit {
-  public city: string = ''
-  constructor(public modalController: ModalController) {
+  public city: string = '';
+  public url : string = environment.apia + "users/addresses";
+  constructor(public modalController: ModalController, public filters : FiltersServicesService, public http: DirectionsEnvioService) {
    
    }
 
   ngOnInit() {
+    this.filters.getToken().then((envio)=>{
+      console.log('token: ',envio)
+      const headers = {
+        'Content-Type' : 'application/json',
+        'Accept-Language': 'es',
+        'Authorization' : envio
+      }
+      this.http.getAddress(this.url, {headers: headers}).subscribe((data: any)=>{
+        console.log(data)
+      })
+    })
   }
 
   async presentModal() {
--- src/app/pages/directions-envio/services/directions-envio.service.ts
+++ src/app/pages/directions-envio/services/directions-envio.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DirectionsEnvioService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getAddress(url, headers){
+    return this.http.get(url, headers)
+  }
+
 }
